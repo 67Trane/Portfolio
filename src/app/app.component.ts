5594600caf9d5a376891d37ce4825ce4f3f6d901
67Trane <144054852+67Trane@users.noreply.github.com>
2024-11-11 18:08:48 +0000

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,13 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsetComponent } from "./skillset/skillset.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { TestimonialComponent } from "./testimonial/testimonial.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, AboutMeComponent, SkillsetComponent, ProjectsComponent, TestimonialComponent],
+  imports: [HeaderComponent, AboutMeComponent, SkillsetComponent, ProjectsComponent, TestimonialComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   template: `<p>{{ title() }}</p>`
