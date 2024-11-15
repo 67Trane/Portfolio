703eebe0758181a93d1d366f6fea1169e2fd7ba3
Trane <Trane_67@hotmail.de>
2024-11-16 00:44:41 +0100

    update

--- src/app/projects/project-card/project-card.component.ts
+++ src/app/projects/project-card/project-card.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
 export class ProjectCardComponent {
+
+  @Input() isModalOpen: boolean = true
+  @Output() isModalOpenChange = new EventEmitter<boolean>();
+
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen;
+    this.isModalOpenChange.emit(this.isModalOpen); // Signal an den Parent
+  }
+
 
 }
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Output } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ProjectCardComponent } from "./project-card/project-card.component";
 
 @Component({
@@ -9,33 +9,36 @@ import { ProjectCardComponent } from "./project-card/project-card.component";
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
 
-  isModalOpen: boolean = false;
   hoveredIndex: number | null = null;
   picIndex: any = 1
+  ismobile: boolean = false;
+  isOpenPorject: boolean = false;
 
   projects = [
     {
-      "name": "Join",
+      "name": "Join (Kanban Board)",
       "tools": ["HTML", "CSS", "Javascript", "Firebase"],
       "picture": "assets/imgs/join.jpeg"
     },
     {
-      "name": "NightStalker",
+      "name": "NightStalker (Game)",
       "tools": ["HTML", "CSS", "Javascript"],
       "picture": "assets/imgs/nightstalker.png"
     },
     {
-      "name": "DA Bubble",
+      "name": "DA Bubble (Buessniss App)",
       "tools": ["Angular", "Firebase", "TypeScript"],
       "picture": "assets/imgs/da-bubble.jpeg"
     },
   ]
 
-  toggleModal() {
-    this.isModalOpen = !this.isModalOpen;
+
+  ngOnInit(): void {
+    this.onMobile(); // Beim Initialisieren prüfen
   }
+
 
 
   onMouseOver(index: number) {
@@ -47,5 +50,21 @@ export class ProjectsComponent {
     this.hoveredIndex = null;
   }
 
+  onMobile() {
+    this.ismobile = window.innerWidth < 768;
+  }
+
+  @HostListener('window:resize') 
+  onResize() {
+    this.onMobile(); 
+  }
+
+  openProject() {
+    this.isOpenPorject = true
+  }
+
+  handleModalChange(newState: boolean) {
+    this.isOpenPorject = newState;
+  }
 
 }
