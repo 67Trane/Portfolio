45838a874ea86dac7f89d284180c696f06251806
Trane <Trane_67@hotmail.de>
2024-11-17 01:08:08 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, input } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component} from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsetComponent } from "./skillset/skillset.component";
@@ -7,14 +6,21 @@ import { ProjectsComponent } from "./projects/projects.component";
 import { TestimonialComponent } from "./testimonial/testimonial.component";
 import { ContactComponent } from "./contact/contact.component";
 import { FooterComponent } from "./footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, AboutMeComponent, SkillsetComponent, ProjectsComponent, TestimonialComponent, ContactComponent, FooterComponent],
+  imports: [HeaderComponent, AboutMeComponent, SkillsetComponent, ProjectsComponent, TestimonialComponent, ContactComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  mouseX: number = 0;
+  mouseY: number = 0;
 
+  onMouseMove(event: MouseEvent) {
+    this.mouseX = event.clientX;  // X-Position relativ zum Fenster
+    this.mouseY = event.clientY;  // Y-Position relativ zum Fenster
+  }
 }
