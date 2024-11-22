c72f3ca993114246a723ac0037461c15c4bd8110
Trane <Trane_67@hotmail.de>
2024-11-23 00:22:42 +0100

    update

--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -9,37 +9,48 @@ import { Component } from '@angular/core';
   styleUrl: './testimonial.component.scss'
 })
 export class TestimonialComponent {
+
   testimonials = [
     {
       text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas eos adipisci illo deserunt nihil labore illum, eveniet debitis nemo dignissimos? Minima quae quasi ex hic blanditiis molestias doloremque adipisci iure.',
-      author: 'Hans Jürgen'
+      author: 'Hans Jürgen1'
     },
     {
       text: 'Ein zweites Testimonial. Ipsum dolor sit amet, consectetur adipiscing elit.',
-      author: 'Anna Schmidt'
+      author: 'Anna Schmidt2'
     },
     {
       text: 'Und hier ein drittes Testimonial mit einem anderen Textinhalt.',
-      author: 'Peter Müller'
+      author: 'Peter Müller3'
+    },
+    {
+      text: 'Und hier ein drittes Testimonial mit einem anderen Textinhalt.',
+      author: 'Peter Müller4'
     }
   ];
 
   currentIndex = 0;
 
-  get previousIndex() {
-    return (this.currentIndex - 1 + this.testimonials.length) % this.testimonials.length;
+  get currentTranslate(): string {
+    if(window.innerWidth < 768) {
+      return `translateX(-${this.currentIndex * 100}%)`;
+    } else {
+      return `translateX(-${this.currentIndex * 120}%)`;
+    }
+    
   }
 
-  get nextIndex() {
-    return (this.currentIndex + 1) % this.testimonials.length;
+  right() {
+    if (this.currentIndex < this.testimonials.length - 1) {
+      this.currentIndex++;
+    }
   }
 
-  previous() {
-    this.currentIndex = this.previousIndex;
+  left() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
   }
 
-  next() {
-    this.currentIndex = this.nextIndex;
-  }
 }
 
