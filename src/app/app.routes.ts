ba7e28e8826fea7ace4e5ef915c49975124f7cbb
Trane <Trane_67@hotmail.de>
2024-11-25 00:39:27 +0100

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 export const routes: Routes = [
     { path: '', component: AppComponent },
+    { path: 'impressum', component: ImpressumComponent }
 ];
