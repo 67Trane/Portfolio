6dc1e6677f59c2a52241a81b019e78569eacbade
Trane <Trane_67@hotmail.de>
2024-11-25 01:49:15 +0100

    big update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { HomeComponent } from './home/home.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }, // Hauptseite
-    { path: 'impressum', component: ImpressumComponent } // Impressum-Seite
+    { path: '', component: HomeComponent },
+    { path: 'impressum', component: ImpressumComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent } 
   ];
