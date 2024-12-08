8da5214e682e79d80e3d1cb54e7336259ec455fa
Trane <Trane_67@hotmail.de>
2024-12-08 22:50:49 +0100

    update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -14,6 +14,7 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class ContactComponent {
   contactname:string = "";
+  messageIsSend: boolean = false;
 
   http = inject(HttpClient);
 
@@ -43,6 +44,7 @@ export class ContactComponent {
 
   onSubmit(ngForm: NgForm) {
     if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.messageIsSend = true
       this.contactname = this.contactData.name
       this.sendMessage()
       this.http.post(this.post.endPoint, this.post.body(this.contactData))
