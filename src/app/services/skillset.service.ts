37f906cc13f8b3ac213353b0b2ba89036b8404da
Trane <Trane_67@hotmail.de>
2025-10-26 19:10:49 +0100

    icon files comments

--- src/app/services/skillset.service.ts
+++ src/app/services/skillset.service.ts
@@ -8,30 +8,41 @@ export class SkillsetService {
 
 
   iconFiles = [
-    { name: "Angular", file: "Angular.svg" },
-    { name: "API", file: "Api.svg" },
-    { name: "CSS", file: "css.svg" },
-    { name: "Firebase", file: "Firebase.svg" },
-    { name: "Material Design", file: "Frame.svg" },
-    { name: "Git", file: "git.svg" },
+    // 🧱 Frontend
     { name: "HTML", file: "html.svg" },
+    { name: "CSS", file: "css.svg" },
     { name: "JavaScript", file: "Javascript.svg" },
-    { name: "Scrum", file: "scrum.svg" },
     { name: "TypeScript", file: "typescript.svg" },
+    { name: "Angular", file: "Angular.svg" },
+    { name: "Material Design", file: "Frame.svg" },
+
+    // 🔧 Tools & Collaboration
+    { name: "Git", file: "git.svg" },
+    { name: "Scrum", file: "scrum.svg" },
+    { name: "API", file: "Api.svg" },
+    { name: "Firebase", file: "Firebase.svg" },
+
+    // ⚙️ Backend & Databases
     { name: "Python", file: "python.svg" },
     { name: "Django", file: "django.svg" },
     { name: "JWT", file: "jwt.svg" },
-    { name: "PostgresSQL", file: "postgresql.svg" },
-    { name: "Docker", file: "docker.svg" },
-    { name: "Debian", file: "debian.svg" },
-    { name: "Gunicorn", file: "gunicorn.svg" },
-    { name: "Linux", file: "linux.svg" },
-    { name: "Nginx", file: "nginx.svg" },
-    { name: "Rasppery Pi", file: "raspi.svg" },
-    { name: "Redis", file: "redis.svg" },
+    { name: "PostgreSQL", file: "postgresql.svg" },
     { name: "SQLite", file: "sqlite.svg" },
+    { name: "Redis", file: "redis.svg" },
+    { name: "Gunicorn", file: "gunicorn.svg" },
+
+    // 🐳 DevOps & Systems
+    { name: "Docker", file: "docker.svg" },
+    { name: "Nginx", file: "nginx.svg" },
+    { name: "Debian", file: "debian.svg" },
+    { name: "Linux", file: "linux.svg" },
+    { name: "Raspberry Pi", file: "raspi.svg" },
+
+    // 💡 Personal Skills
     { name: "Growth Mindset", file: "growthmindset.svg" }, // Caution! Growth has to be the last one
   ];
+
+
 
   iconFolderPath = '/assets/icons/skillset-icons/';
 
