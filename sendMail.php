26eba1e56c1c41cb7ea4a53cefaa6b7a75e4c576
Trane <Trane_67@hotmail.de>
2025-11-28 19:53:02 +0100

    fix email

--- sendMail.php
+++ sendMail.php
@@ -18,7 +18,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $name = $params->name;
             $message = $params->message;
     
-            $recipient = 'Trane_67@hotmail.de';  
+            $recipient = 'info@mehmet-deliaci.net';
             $subject = "Contact From <$email>";
             $message = "From:" . $name . "<br>" . $message ;
     
